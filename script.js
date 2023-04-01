c2eb65f0bd12922e5ac85edb98bcb8782a576421
Davidtt083 <davidtt083@gmail.com>
2023-03-31 21:27:48 -0600

    test

--- script.js
+++ script.js
@@ -13,3 +13,37 @@ const onProgress = (event) => {
   }
 };
 document.querySelector('model-viewer').addEventListener('progress', onProgress);
+
+
+
+const playButton = document.querySelector('#boton1');
+const modelViewer = document.querySelector('model-viewer');
+playButton.addEventListener('click', () => {
+  // modelViewer.currentTime = 0; // reinicia la animación si ya se estaba reproduciendo
+  modelViewer.play({ repetitions: 1 });
+});
+
+
+function mostrarp(divNum) {
+  document.getElementById("text1").classList.add("hide");
+  document.getElementById("text2").classList.add("hide");
+  document.getElementById("text3").classList.add("hide");
+  /* document.getElementById("text4").classList.add("hide");
+   document.getElementById("text5").classList.add("hide");
+   document.getElementById("text6").classList.add("hide");
+   document.getElementById("text7").classList.add("hide");*/
+
+
+  // Show the specific div
+  document.getElementById("text" + divNum).classList.remove("hide");
+}
+
+function cerrar() {
+  document.getElementById("text1").classList.add("hide");
+  document.getElementById("text2").classList.add("hide");
+  document.getElementById("text3").classList.add("hide");
+  /* document.getElementById("text4").classList.add("hide");
+   document.getElementById("text5").classList.add("hide");
+   document.getElementById("text6").classList.add("hide");
+   document.getElementById("text7").classList.add("hide");*/
+}
